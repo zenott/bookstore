16b108d1ee19c2fe3acd8b78c4134446f9f5b6ec
zenott <mblevel@gmail.com>
2019-08-01 12:41:10 +0200

    Implement App component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BookList from './BookList';
+import BookForm from './BookForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BookList />
+      <BookForm />
+    </div>
+  );
+}
+
+export default App;
