e240565f6a39fab599abf98bac48d5b458fe8189
zenott <mblevel@gmail.com>
2019-08-05 21:23:41 +0200

    Updates

---
+++ precache-manifest.a2d038146a9c3f43fabe86501ecf6a61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7273f1f1402a0e55e600a61b03bddbf6",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "a05f61107f2d48ef666d",
+    "url": "/bookstore/static/css/main.fb0cf9d7.chunk.css"
+  },
+  {
+    "revision": "7ed7096ecefdcc6bb2e6",
+    "url": "/bookstore/static/js/2.e743cbf6.chunk.js"
+  },
+  {
+    "revision": "a05f61107f2d48ef666d",
+    "url": "/bookstore/static/js/main.ba8086b0.chunk.js"
+  },
+  {
+    "revision": "e3e84e75878dc5d45b9e",
+    "url": "/bookstore/static/js/runtime~main.2e2d1535.js"
+  }
+]);
